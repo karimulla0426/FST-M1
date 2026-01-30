fc07fc339b9030611b97b2b1c3577b88cb82b7ad
karimulla0426 <karimulla.kale.sahebgari@ibm.com>
2026-01-30 16:47:45 +0530

    Add files via upload

---
+++ Python/Activities/Activity 1.2.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+current_year = 2026
+year_turn_100 = current_year + (100 - age)
+
+print(f"Hi {name}! You will turn 100 years old in the year {year_turn_100}.")
